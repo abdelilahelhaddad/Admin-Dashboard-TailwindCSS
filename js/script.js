e932b120d88c5d86cf70fd01646bea15aa267382
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2021-08-02 14:19:53 +0100

    :fire: :art: Second Commit Desktop Layout :sparkles: :globe_with_meridians:

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Profile dropdown menu
+
 const dropdown = document.querySelector('.dropdown');
 const dropdownMenu = document.querySelector('.dropdown-menu');
 
@@ -5,3 +7,24 @@ dropdown.addEventListener('click', (e) => {
   e.preventDefault();
   dropdownMenu.classList.toggle('hidden');
 });
+
+
+//Sales Overview Chart
+
+
+var options = {
+  chart: {
+    type: 'bar'
+  },
+  series: [{
+    name: 'Sales',
+    data: [74, 42, 37, 55, 49, 60, 70, 91, 111, 230, 470, 588]
+  }],
+  xaxis: {
+    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+  }
+}
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+chart.render();
